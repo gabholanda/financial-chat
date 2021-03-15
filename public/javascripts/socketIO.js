064f41eba43e7ef9aede7f9914a672613636ea7e
Gabriel Holanda <gabriel_ebot@hotmail.com>
2021-03-15 15:04:41 -0300

    Added some styling, messages limitations and memory caching

--- public/javascripts/socketIO.js
+++ public/javascripts/socketIO.js
@@ -6,7 +6,6 @@ const input = document.getElementById("message");
 const nickname = document.getElementById("nickname");
 
 if (form) {
-  console.log("created Form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (input.value) {
@@ -16,9 +15,19 @@ if (form) {
   });
 }
 
-socket.on("chat message", function (msg) {
+socket.on("chat message", (data) => {
+  if (data.update) messages.removeChild(messages.childNodes[0]);
   var item = document.createElement("li");
-  item.textContent = msg;
+  item.textContent = data.msg;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on("load old messages", (userMessages) => {
+  userMessages.forEach((message) => {
+    var item = document.createElement("li");
+    item.textContent = message;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 });
--- services/socket.service.js
+++ services/socket.service.js
@@ -1,9 +1,26 @@
+const messages = [];
+
 const start = (server) => {
   const io = require("socket.io")(server);
+
   io.on("connection", (socket) => {
-    console.log("a user connected");
+    // Load all old messages saved in memory on connection
+    socket.emit("load old messages", messages);
+
     socket.on("chat message", (msg) => {
-      io.emit("chat message", msg);
+      const command = msg.match(/\/stocks /g);
+      if (command) {
+        const stockCode = msg.split(" ")[1];
+        // Fire bot and API
+      } else {
+        messages.push(msg);
+        if (messages.length >= 50) {
+          messages.slice(0, 1);
+          io.emit("chat message", { msg, update: true });
+        } else {
+          io.emit("chat message", { msg, update: false });
+        }
+      }
     });
   });
 };
