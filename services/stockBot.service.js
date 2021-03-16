bcc9f284d6aea99c20e0ce42a3747888117d460b
Gabriel Holanda <gabriel_ebot@hotmail.com>
2021-03-15 22:51:43 -0300

    Adding emitStock functionality to the bot and initializing IO on it

--- services/stockBot.service.js
+++ services/stockBot.service.js
@@ -2,21 +2,49 @@ const axios = require("axios");
 const csv = require("csvtojson/v2");
 const socketBot = {};
 const queueManager = require("../connector/rabbit");
+let IO;
+const botName = "StockBot";
 
-socketBot.requestCSV = (endpoint, stockCode) =>
+socketBot.requestCSV = (stockCode) =>
   new Promise((resolve, reject) => {
     axios
-      .get(`${endpoint}?s=${stockCode.toLowerCase()}&f=sd2t2ohlcv&h&e=csv`)
-      .then((response) => {
-        resolve(socketBot.convertCSVToJson(response.data));
-      })
+      .get(
+        `${
+          process.env.STOCK_URL
+        }?s=${stockCode.toLowerCase()}&f=sd2t2ohlcv&h&e=csv`
+      )
+      .then((response) => resolve(socketBot.convertCSVToJson(response.data)))
       .catch((err) => {
+        console.error(err);
         reject(err);
       });
   });
 
 socketBot.convertCSVToJson = async (csvFile) => {
   return await csv({ trim: true, delimiter: "," }).fromString(csvFile);
+};
+
+socketBot.emitStock = async (stockCode) => {
+  const parsedCsvArr = await socketBot.requestCSV(stockCode);
+  let msg;
+  if (!parsedCsvArr || !parsedCsvArr.length) {
+    msg = `${botName}: Something went wrong while trying to get this quote ${stockCode}`;
+    IO.emit("chat message", { msg, update: false });
+  }
+
+  const jsonData = parsedCsvArr[0];
+  // I have no idea on how to calculate quotes
+  // so I am just putting a random value from the .csv file here
+  if (jsonData.Open == "N/D") {
+    msg = `${botName}: I haven't found a quote with this stock code`;
+  } else {
+    msg = `${botName}: ${jsonData.Symbol} quote is $${jsonData.Open} per share`;
+  }
+  IO.emit("chat message", { msg, update: false });
+};
+
+socketBot.startIO = (io) => {
+  IO = io;
 };
 
 socketBot.publishToQueue = (queue, content) => {
